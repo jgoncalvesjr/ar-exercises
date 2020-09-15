0e5039693dc1f8ea181e0309057a8dfcf40763be
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-09-15 00:11:35 +0000

    Complete exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -4,10 +4,20 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :mens_apparel, :womens_apparel, inclusion: { in: [true, false] }
   validate :mandatory_apparel
+  before_destroy :empty_store?
+
+  private
 
   def mandatory_apparel
     if mens_apparel == false && womens_apparel == false
       errors.add(:mens_apparel, :womens_apparel, "At least one apparel must be true!")
+    end
+  end
+
+  def empty_store?
+    if employees.count > 0
+      errors.add(:employees, "Store is not empty, cannot delete it!")
+      false
     end
   end
 end
